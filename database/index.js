4fdebf2f761a8148524a72a72e893af22cb16994
Miguel Guerrero <mgangsal@outlook.com>
2021-06-28 17:51:01 -0500

    Added a database

--- database/index.js
+++ database/index.js
@@ -1,1 +1,15 @@
 // TODO Add database support
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.DB_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", function () {
+	console.error("Connection failed.\n");
+});
+db.once("open", function () {
+	console.log("Database successfully opened");
+});
