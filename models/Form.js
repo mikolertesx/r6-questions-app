d6fb46cfe4b525fe3d679535ee65078ec6841544
Miguel Guerrero <mgangsal@outlook.com>
2021-06-29 19:37:14 -0500

    Form Model now has methods to add questions.

--- models/Form.js
+++ models/Form.js
@@ -1,10 +1,63 @@
 import Mongoose from 'mongoose'
+import Questions from 'models/questions'
+import types from 'constants/options'
 
 const formSchema = new Mongoose.Schema({
   author: Mongoose.Types.ObjectId,
-  questions: [{ type: ObjectId, ref: 'Question' }],
+  questions: [{ type: Mongoose.Types.ObjectId, ref: 'Question' }],
 })
 
-const Form = Mongoose.model('Question', formSchema)
+formSchema.methods.addMultipleQuestion = async function (text, options) {
+  const newQuestion = await Questions.create({
+    text,
+    options,
+    type: types.MULTIPLE,
+  })
+  this.questions.push(newQuestion._id)
+  return newQuestion
+}
+
+formSchema.methods.addRangeQuestion = async function (
+  text,
+  minRange,
+  maxRange
+) {
+  const newQuestion = await Questions.create({
+    text,
+    minRange,
+    maxRange,
+    type: types.RANGE,
+  })
+  this.questions.push(newQuestion._id)
+  return newQuestion
+}
+
+formSchema.methods.addOpenQuestion = async function (text) {
+  const newQuestion = await Questions.create({ text, type: types.OPEN })
+  this.questions.push(newQuestion._id)
+  return newQuestion
+}
+
+formSchema.methods.addCheckboxQuestion = async function (text, options) {
+  const newQuestion = await Questions.create({
+    text,
+    options,
+    type: types.CHECKBOX,
+  })
+  this.questions.push(newQuestion._id)
+  return newQuestion
+}
+
+formSchema.methods.addBooleanQuestion = async function (text) {
+  const newQuestion = await Questions.create({
+    text,
+    options: ['YES', 'NO'],
+    type: types.CHECKBOX,
+  })
+  this.questions.push(newQuestion._id)
+  return newQuestion
+}
+
+const Form = Mongoose.model('Form', formSchema)
 
 export default Form
