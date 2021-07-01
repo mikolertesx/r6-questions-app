468d2f23952b89b24c4ed7c15d4d54c022e3cad3
Carlos D. Jaime <ing.carlosjaime@outlook.com>
2021-06-30 23:43:12 -0500

    Added styles to the open question gen component

--- components/open-question-gen/index.jsx
+++ components/open-question-gen/index.jsx
@@ -1,13 +1,19 @@
+import styles from './styles.module.scss'
+
 const OpenQuestionGen = ({ setData, questionData }) => {
   return (
-    <>
-      <label htmlFor="">Add your question</label>
-      <textarea
+    <div className={styles['question-gen-container']}>
+      <label className={styles.label} htmlFor="">
+        Add your question
+      </label>
+      <input
+        className={styles.input}
+        type="text"
         onChange={(e) =>
           setData({ ...questionData, questionText: e.target.value })
         }
       />
-    </>
+    </div>
   )
 }
 
