085293cb0c673e19d78e955176c26c038df7ca14
Miguel Guerrero <mgangsal@outlook.com>
2021-07-01 16:22:56 -0500

    Refactored api can now be called from inside the server

--- pages/api/auth/verify.js
+++ pages/api/auth/verify.js
@@ -5,6 +5,15 @@ const verifySchema = {
   username: '[String] The name of the user you are trying to create.',
   password: '[String] The password.',
 }
+/**
+ * verifies that the token is from a user.
+ * @param  {String} token Unique identifier
+ * @returns {[data, error]} An array of data ({token, username, _id}), and error object.
+ */
+export async function verifyToken(token) {
+  const [data, error] = await User.verifyToken(token)
+  return [data, error]
+}
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
@@ -26,7 +35,7 @@ export default async function handler(req, res) {
     })
   }
 
-  const [data, error] = await User.verifyToken(token)
+  const [data, error] = await verifyToken(token)
   if (error) {
     return res.status(400).json({
       error: error.message,
