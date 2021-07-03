71f23f5f342c031df19aa0942af1969b12b436b0
Miguel Guerrero <mgangsal@outlook.com>
2021-07-02 19:15:58 -0500

    Added answers route

--- pages/api/forms/[...slug].js
+++ pages/api/forms/[...slug].js
@@ -24,7 +24,26 @@ export const createQuestion = async (formId) => {
   }
 }
 
+export const getAnswers = async (formId) => {
+  try {
+    const form = await Form.findById(formId)
+    if (!form) {
+      return [null, new Error("Couldn't find Form.")]
+    }
+
+    const answers = await form.clientsAnswers()
+    return [answers, null]
+  } catch (err) {
+    console.error(err)
+    return [null, err]
+  }
+}
+
 const findFormRoute = async (req, res) => {
+  const { method } = req
+  if (method !== 'GET') {
+    return res.json({ error: 'This route only allows for get the get method.' })
+  }
   const { slug } = req.query
   const [id] = slug
   try {
@@ -37,17 +56,42 @@ const findFormRoute = async (req, res) => {
 }
 
 const createQuestionRoute = async (req, res) => {
+  const { method } = req
   const { slug } = req.query
   const [id, action] = slug
-  if (action !== 'add-question') {
+	
+  if (action !== 'add-question' && action !== 'see-answers') {
     return res.json({ error: `You can\'t use action "${action}"` })
   }
-  const [data, error] = await createQuestion(id)
-  if (error) {
-    return res.json({ error: error.message })
-  }
 
-  return res.json(data)
+  switch (action) {
+    case 'add-question':
+      if (method !== 'GET') {
+        return res.json({
+          error: 'This route only allows for get the get method.',
+        })
+      }
+      const [data, error] = await createQuestion(id)
+      if (error) {
+        return res.json({ error: error.message })
+      }
+      return res.json(data)
+    case 'see-answers':
+      if (method !== 'GET') {
+        return res.json({
+          error: 'This route only allows for get the get method.',
+        })
+      }
+
+      const [answersData, answersError] = await getAnswers(id)
+      if (answersError) {
+        return res.json({
+          error: answersError,
+        })
+      }
+
+      return res.json(answersData)
+  }
 }
 
 const deleteQuestionRoute = async (req, res) => {
@@ -79,11 +123,7 @@ const deleteQuestionRoute = async (req, res) => {
 }
 
 export default async function handler(req, res) {
-  const { method } = req
   const { slug } = req.query
-  if (method !== 'GET') {
-    return res.json({ error: 'This route only allows for get the get method.' })
-  }
 
   if (slug.length === 1) {
     return findFormRoute(req, res)
