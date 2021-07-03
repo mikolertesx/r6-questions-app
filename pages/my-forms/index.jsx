9dfef9331283a5dfe455cf79869e7489296d8a6c
Carlos D. Jaime <ing.carlosjaime@outlook.com>
2021-07-03 13:29:41 -0500

    Connected the my-forms page to the store

--- pages/my-forms/index.jsx
+++ pages/my-forms/index.jsx
@@ -1,43 +1,46 @@
-import { useState } from 'react'
-import ButtonControls from 'components/button-controls'
+import { connect, useDispatch } from 'react-redux'
+import { addForm } from 'store/formsReducer'
 import { FormPreview } from 'components/form-preview'
 import styles from './styles.module.scss'
 import Navbar from 'components/landing-navbar'
 
-const MyFormsPage = () => {
-  const [currentForms, setCurrentForms] = useState([])
+const MyFormsPage = ({ forms, addForm }) => {
   const addNewForm = () => {
-    const newArray = [...currentForms]
-    newArray.push({})
-    setCurrentForms(newArray)
+    const index = Math.random()
+    addForm(index)
+    console.log(forms)
   }
   return (
     <>
-    <Navbar/>
-    <div className={styles.container}>
-      <h1>My Forms</h1>
-      {currentForms.length > 0 ? (
-        <p>You can modify each form if you click on the title.</p>
-      ) : (
-        <p>
-          You can manage your forms from here, click on the plus button to get
-          started.
-        </p>
-      )}
-      {currentForms.length > 0 &&
-        currentForms.map((form, index) => (
-          <FormPreview
-            key={`form-${index}`}
-            index={index}
-            currentForms={currentForms}
-            setCurrentForms={setCurrentForms}
-          />
-        ))}
-      <button onClick={addNewForm}><strong>Add a Form +</strong></button>
-    </div>
+      <Navbar />
+      <div className={styles.container}>
+        <h1>My Forms</h1>
+        {Object.keys(forms).length > 0 ? (
+          <p>You can modify each form if you click on the title.</p>
+        ) : (
+          <p>
+            You can manage your forms from here, click on the plus button to get
+            started.
+          </p>
+        )}
+        {Object.values(forms).length > 0 &&
+          Object.values(forms).map((form, index) => (
+            <FormPreview key={`form-${index}`} index={index} />
+          ))}
+        <button onClick={addNewForm}>
+          <strong>Add a Form +</strong>
+        </button>
+      </div>
     </>
-
   )
 }
 
-export default MyFormsPage
+const MapStateToProps = ({ forms }) => ({
+  forms,
+})
+
+const MapDispatchToProps = (dispatch) => ({
+  addForm: (formId) => dispatch(addForm(formId)),
+})
+
+export default connect(MapStateToProps, MapDispatchToProps)(MyFormsPage)
