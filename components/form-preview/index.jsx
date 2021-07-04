ec647a27a59b16cc87d3b10eb0f3626319f9e60e
Carlos D. Jaime <ing.carlosjaime@outlook.com>
2021-07-04 12:39:57 -0500

    Add share button to the form preview component

--- components/form-preview/index.jsx
+++ components/form-preview/index.jsx
@@ -4,9 +4,21 @@ import { connect } from 'react-redux'
 import { removeForm } from 'store/formsReducer'
 import { RiDeleteBin5Fill } from 'react-icons/ri'
 import { VscPreview } from 'react-icons/vsc'
+import { GrShareOption } from 'react-icons/gr'
 import styles from './styles.module.scss'
 
 const FormPreview = ({ formId, title, removeForm }) => {
+  const FORM_URI = `http://localhost:3000/form/${formId}`
+
+  const copyFormUrl = () => {
+    const el = document.createElement('input')
+    el.value = FORM_URI
+    document.body.appendChild(el)
+    el.select()
+    document.execCommand('copy')
+    document.body.removeChild(el)
+  }
+
   const router = useRouter()
   return (
     <div className={styles.container}>
@@ -14,6 +26,10 @@ const FormPreview = ({ formId, title, removeForm }) => {
         <h1 className={styles['form-title']}>{title}</h1>
       </Link>
       <div className={styles.controls}>
+        <GrShareOption
+          className={styles['control-btn']}
+          onClick={() => copyFormUrl()}
+        />
         <VscPreview
           className={styles['control-btn']}
           onClick={() => {
