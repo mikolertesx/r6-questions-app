c0184fbe2b371529fb4a1237643551a04c7dedae
Carlos D. Jaime <ing.carlosjaime@outlook.com>
2021-07-04 11:53:02 -0500

    Added a preview functionality for the forms

--- components/form-preview/index.jsx
+++ components/form-preview/index.jsx
@@ -1,16 +1,24 @@
 import Link from 'next/Link'
+import { useRouter } from 'next/router'
 import { connect } from 'react-redux'
 import { removeForm } from 'store/formsReducer'
 import { RiDeleteBin5Fill } from 'react-icons/ri'
+import { VscPreview } from 'react-icons/vsc'
 import styles from './styles.module.scss'
 
 const FormPreview = ({ formId, title, removeForm }) => {
+  const router = useRouter()
   return (
     <div className={styles.container}>
       <Link href={`/my-forms/${formId}`}>
         <h1 className={styles['form-title']}>{title}</h1>
       </Link>
       <div className={styles.controls}>
+        <VscPreview
+          onClick={() => {
+            router.push(`/preview/${formId}`)
+          }}
+        />
         <RiDeleteBin5Fill
           className={styles['delete-btn']}
           onClick={() => {
--- components/question-card/index.jsx
+++ components/question-card/index.jsx
@@ -2,63 +2,68 @@ import { useEffect, useState } from 'react'
 import style from './styles.module.scss'
 
 const QuestionCard = (props) => {
-
   const [question, setQuestion] = useState(props.question)
 
-  if (question.type === "OPEN") {
-      
-      return (
-        <div className={style.question}>
-          <h2>{`${question.questionText}`}</h2>
-          <input placeholder="Enter your answer here"></input>
-        </div>
-      )
+  if (question.type === 'OPEN') {
+    return (
+      <div className={style.question}>
+        <h2>{`${question.questionText}`}</h2>
+        <input placeholder="Enter your answer here"></input>
+      </div>
+    )
   }
 
-  if (question.type === "MULTIPLE") {
-      
+  if (question.type === 'MULTIPLE') {
     return (
-      <div  className={style.question}>
+      <div className={style.question}>
         <h2>{`${question.questionText}`}</h2>
         <label> Pick an option</label>
         <form>
-            {Object.values(question.options).map((option, index)=>{
-               return <>
-                <input className={style.option} key={index} name={question.questionText} type="radio"/><label key={`l-${index}`}>{option}</label>
-                </>
-            })}
+          {Object.values(question.options).map((option, index) => {
+            return (
+              <>
+                <input
+                  className={style.option}
+                  key={index}
+                  name={question.questionText}
+                  type="radio"
+                />
+                <label key={`l-${index}`}>{option}</label>
+              </>
+            )
+          })}
         </form>
       </div>
     )
   }
 
-if (question.type === "CHECKBOX") {
-      
+  if (question.type === 'CHECKBOX') {
     return (
-      <div  className={style.question}>
-         <h2>{`${question.questionText}`}</h2>
+      <div className={style.question}>
+        <h2>{`${question.questionText}`}</h2>
         <label> Pick one or more options</label>
         <div>
-            {Object.values(question.options).map((option, index)=>{
-               return <>
-                <input className={style.option} key={index} type="checkbox"/><label key={`l-${index}`}>{option}</label>
-                </>
-            })}
+          {Object.values(question.options).map((option, index) => {
+            return (
+              <>
+                <input className={style.option} key={index} type="checkbox" />
+                <label key={`l-${index}`}>{option}</label>
+              </>
+            )
+          })}
         </div>
       </div>
     )
-  }  
+  }
 
-if (question.type === "RANGE") {
-      
+  if (question.type === 'RANGE') {
     return (
-      <div  className={style.question}>
+      <div className={style.question}>
         <h2>{`${question.questionText}`}</h2>
-        <input type="number" min="0" max={question.rangeLimit}/>
+        <input type="number" min="0" max={question.rangeLimit} />
       </div>
     )
-  } 
-
+  }
 }
 
 export default QuestionCard
